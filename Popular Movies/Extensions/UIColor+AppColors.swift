b6e785eec56583ffc70062e3d26a0e0431f4f540
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-06 14:26:45 +0300

    added UIColor ext for easily getting apps color set

---
+++ Popular Movies/Extensions/UIColor+AppColors.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+AppColors.swift
+//  Popular Movies
+//
+//  Created by Алексей Павленко on 05.09.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let mainColor = UIColor(named: "main")!
+    static let secondaryColor = UIColor(named: "secondary")!
+}
+
