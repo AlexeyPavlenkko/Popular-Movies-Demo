d46a18a3be728e24dbfb42d28044f04c176c365a
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-06 14:27:51 +0300

    added decodable Movie struct to convert from retrieved json data from network response

---
+++ Popular Movies/Models/Movie.swift
@@ -0,0 +1,33 @@
+//
+//  Movie.swift
+//  Popular Movies
+//
+//  Created by Алексей Павленко on 05.09.2022.
+//
+
+import Foundation
+
+struct MoviesData: Decodable {
+    let movies: [Movie]
+    
+    private enum CodingKeys: String, CodingKey {
+        case movies = "results"
+    }
+}
+
+struct Movie: Decodable {
+    let title: String?
+    let year: String?
+    let rate: Double?
+    let posterImage: String?
+    let backdropImage: String?
+    let overview: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case title, overview
+        case year = "release_date"
+        case rate = "vote_average"
+        case posterImage = "poster_path"
+        case backdropImage = "backdrop_path"
+    }
+}
